d66de7af9dfdd9600b61a84f2cdfdf89f68db049
Can Gündüz <cangunduz0001@gmail.com>
2024-09-19 00:39:58 +0300

    Search UI | BottomSheetModal v2 | 1.0.2.2

--- components/BottomSheet.tsx
+++ components/BottomSheet.tsx
@@ -1,6 +1,14 @@
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
 import React, { forwardRef, useCallback, useMemo } from "react";
-import { BottomSheetBackdrop, BottomSheetModal, useBottomSheetModal } from "@gorhom/bottom-sheet";
+import {
+  BottomSheetBackdrop,
+  BottomSheetModal,
+  useBottomSheetModal,
+} from "@gorhom/bottom-sheet";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import Colors from "@/constants/Colors";
+import { Link } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
 export type Ref = BottomSheetModal;
 
@@ -16,20 +24,98 @@ const BottomSheet = forwardRef<Ref>((props, ref) => {
     ),
     []
   );
-  const {dismiss} = useBottomSheetModal();
+  const { dismiss } = useBottomSheetModal();
   return (
     <BottomSheetModal
       ref={ref}
       snapPoints={snapPoints}
       overDragResistanceFactor={0}
       backdropComponent={renderBackdrop}
+      // burada handleIndicatorStyle={{display:"none"}} ekleyerek indicator'ı gizler. yani alta kaydırma çubuğunu gizler.
+      //handleIndicatorStyle={{display:"none"}}
     >
-      <View>
-        <Text>Bottom Sheet Content</Text>
-        <Button title="Dismiss" onPress={() => dismiss()} />
+      <View style={{ flex: 1 }}>
+        <View style={styles.toggle}>
+          <TouchableOpacity style={styles.toggleActive}>
+            <Text style={styles.activeText}>Delivery</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.toogleInactive}>
+            <Text style={styles.inactiveText}>Pickup</Text>
+          </TouchableOpacity>
+        </View>
+        <Text style={styles.subheader}>Your Location</Text>
+        <Link href={"/"} asChild>
+          <TouchableOpacity>
+            <View style={styles.item}>
+              <Ionicons name="location" size={20} color={Colors.primary} />
+              <Text style={{ flex: 1 }}>Current location</Text>
+              <Ionicons
+                name="chevron-forward"
+                size={20}
+                color={Colors.primary}
+              />
+            </View>
+          </TouchableOpacity>
+        </Link>
+        <TouchableOpacity onPress={() => dismiss()} style={styles.button}>
+          <Text style={styles.buttonText}>Confirm</Text>
+        </TouchableOpacity>
       </View>
     </BottomSheetModal>
   );
+});
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: Colors.primary,
+    padding: 16,
+    margin: 16,
+    borderRadius: 4,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+  },
+  toggle: {
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 10,
+    marginBottom: 32,
+  },
+  toggleActive: {
+    backgroundColor: Colors.primary,
+    padding: 8,
+    borderRadius: 32,
+    paddingHorizontal: 30,
+  },
+  activeText: {
+    color: "white",
+    fontWeight: "700",
+  },
+  toogleInactive: {
+    padding: 8,
+    borderRadius: 32,
+    paddingHorizontal: 30,
+  },
+  inactiveText: {
+    color: Colors.primary,
+    fontWeight: "700",
+  },
+  subheader: {
+    fontSize: 18,
+    fontWeight: "bold",
+    margin: 16,
+  },
+  item: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 16,
+    borderWidth: 1,
+    borderBottomColor: Colors.grey,
+    gap:8,
+    backgroundColor:'#fff'
+  },
 });
 
 export default BottomSheet;
