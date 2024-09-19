908b0b2133333f212ffdb1b1687a0f20287f3ab4
Can Gündüz <cangunduz0001@gmail.com>
2024-09-20 01:15:56 +0300

    Search UI | Filter Modal v2 | 1.0.3.2

--- app/(modal)/filter.tsx
+++ app/(modal)/filter.tsx
@@ -5,30 +5,109 @@ import {
   TouchableOpacity,
   FlatList,
   ListRenderItem,
+  Button,
 } from "react-native";
 import React from "react";
 import Colors from "@/constants/Colors";
 import { useNavigation } from "expo-router";
-import { categories } from "@/assets/data/home";
-
+import categories from "@/assets/data/filter.json";
+import { Ionicons } from "@expo/vector-icons";
+import BouncyCheckbox from "react-native-bouncy-checkbox";
 interface Category {
   name: string;
   count: number;
   checked?: boolean;
 }
 
+const ItemBox = () => (
+  <>
+    <View style={styles.itemContainer}>
+      <TouchableOpacity style={styles.item}>
+        <View style={styles.item}>
+          <Ionicons
+            name="arrow-down-outline"
+            size={20}
+            color={Colors.primary}
+          />
+          <Text style={{ flex: 1 }}>Sort</Text>
+          <Ionicons name="chevron-forward" size={22} color={Colors.primary} />
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.item}>
+        <View style={styles.item}>
+          <Ionicons name="fast-food-outline" size={20} color={Colors.primary} />
+          <Text style={{ flex: 1 }}>Hygiene rating</Text>
+          <Ionicons name="chevron-forward" size={22} color={Colors.primary} />
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.item}>
+        <View style={styles.item}>
+          <Ionicons name="pricetag-outline" size={20} color={Colors.primary} />
+          <Text style={{ flex: 1 }}>Offers</Text>
+          <Ionicons name="chevron-forward" size={22} color={Colors.primary} />
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.item}>
+        <View style={styles.item}>
+          <Ionicons name="nutrition-outline" size={20} color={Colors.primary} />
+          <Text style={{ flex: 1 }}>Dietary</Text>
+          <Ionicons name="chevron-forward" size={22} color={Colors.primary} />
+        </View>
+      </TouchableOpacity>
+    </View>
+    <Text style={styles.header}>Categories</Text>
+  </>
+);
+
 const Filter = () => {
   const navigation = useNavigation();
-  const renderItem: ListRenderItem<Category> = ({ item }) => {
-    return (
-      <View>
-        <Text>{item.name}</Text>
-      </View>
-    );
+  const [items, setItems] = React.useState<Category[]>(categories);
+
+  const handleClearAll = () => {
+    const updatedItems = items.map((item) => {
+      item.checked = false;
+      return item;
+    });
+    setItems(updatedItems);
   };
+
+  const renderItem: ListRenderItem<Category> = ({ item, index }) => (
+    <View style={styles.row}>
+      <Text style={styles.itemText}>
+        {item.name} ({item.count})
+      </Text>
+      <BouncyCheckbox
+        isChecked={items[index].checked}
+        fillColor={Colors.primary}
+        unfillColor="#fff"
+        iconStyle={{
+          borderColor: Colors.primary,
+          borderRadius: 4,
+          borderWidth: 2,
+        }}
+        innerIconStyle={{ borderColor: Colors.primary, borderRadius: 4 }}
+        onPress={() => {
+          const isChecked = items[index].checked;
+
+          const updatedItems = items.map((item) => {
+            if (item.name === items[index].name) {
+              item.checked = !isChecked;
+            }
+            return item;
+          });
+          setItems(updatedItems);
+        }}
+      />
+    </View>
+  );
   return (
     <View style={styles.container}>
-      <FlatList data={categories} renderItem={renderItem} />
+      <FlatList
+        data={items}
+        renderItem={renderItem}
+        ListHeaderComponent={<ItemBox />}
+      />
+      <View style={{ height: 76 }} />
       <View style={styles.footer}>
         <TouchableOpacity
           style={styles.fullButton}
@@ -71,6 +150,34 @@ const styles = StyleSheet.create({
     color: "#fff",
     fontWeight: "bold",
     fontSize: 18,
+  },
+  itemContainer: {
+    backgroundColor: "#fff",
+    padding: 8,
+    borderRadius: 8,
+    marginBottom: 16,
+  },
+  header: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 16,
+  },
+  item: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 16,
+    paddingVertical: 10,
+    gap: 20,
+    backgroundColor: "#fff",
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10,
+    backgroundColor: "#fff",
+  },
+  itemText: {
+    flex: 1,
   },
 });
 
