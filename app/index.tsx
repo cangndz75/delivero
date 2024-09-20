4c90ee94a2c353fad447c6daf1eb99cf4cbc454d
Can Gündüz <cangunduz0001@gmail.com>
2024-09-20 22:03:54 +0300

    Search UI | Home page v1 | 1.0.5

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,33 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import CustomHeader from '@/components/CustomHeader'
+import { Text, StyleSheet, ScrollView } from "react-native";
+import Categories from "@/components/Categories";
+import Restaurants from "@/components/Restaurants";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Colors from "@/constants/Colors";
 
 const Page = () => {
   return (
-   <CustomHeader/>
-  )
-}
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={{ paddingBottom: 40 }}>
+        <Categories />
+        <Text style={styles.header}>Top picks in your neighborhood</Text>
+        <Restaurants />
+        <Text style={styles.header}>Offers near you</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-export default Page
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.lightGrey,
+    top: 80,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 16,
+    paddingHorizontal: 16,
+  },
+});
+
+export default Page;
